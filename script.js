e8a1d12369c310dcb1eb8878cb49966863f3fe0f
olepak <oleksandr.lepak@gmail.com>
2023-02-22 16:52:58 +0200

    upd

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const ORANGE = "1111";
-const RED = "2222";
-const BLUE = "33333";
-const GREEN = "33333";
-
-console.log(ORANGE);
-let user = ORANGE;
-console.log(ORANGE);
-
-user = "222";
-console.log(user);
-
-let otherUser = "333";
-console.log(otherUser);
-otherUser = 3 + 2;
-console.log(otherUser);
+let myNumber = "73";
+myNumber = Number(myNumber) + 3;
+console.log(myNumber);
