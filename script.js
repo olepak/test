6f564e873b7a73f0f8eff993152c3a75cd5eef8a
olepak <oleksandr.lepak@gmail.com>
2023-04-25 09:43:16 +0300

    upd

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-let num1 = 4;
-num1++;
-console.log(num1);
+let oranges = "2";
+let apples = "3";
+console.log(oranges + apples);
+console.log(typeof oranges);
+console.log(typeof apples);
+let b = +oranges + +apples;
+console.log(b + typeof b);
