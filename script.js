e7eb6eb5945dfe3e74b0286bb67a12731ea8dd09
olepak <oleksandr.lepak@gmail.com>
2023-04-29 13:31:20 +0300

    upd

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-let message = "hello";
-console.log(message + typeof message);
-message = 12345;
-console.log(message + typeof message);
-console.log(Infinity);
-console.log(typeof Infinity);
-let name = "John";
-console.log(`Hello, ${name}`);
-console.log("Hello, ${name}");
+let isGreater = 4 > 1;
+console.log(isGreater);
