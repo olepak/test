5ab16d07ec06f97aa0ca5913e04d4a11bc9e42be
olepak <oleksandr.lepak@gmail.com>
2023-04-29 14:27:58 +0300

    upd

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let isGreater = 4 > 1;
-console.log(isGreater);
+const string = "The revolution will not televized";
+const badString = string;
+console.log(badString);
+const a = 'Would you eat a "fish supper" ';
+console.log(a);
+const b = 'Let it be some "old style" thing';
+console.log(b);
