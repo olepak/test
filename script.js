ce1b5b8dec4bd931b7686e6fa770be1d5c4c2e8c
olepak <oleksandr.lepak@gmail.com>
2023-04-29 15:29:21 +0300

    upd

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const string = "The revolution will not televized";
-const badString = string;
-console.log(badString);
-const a = 'Would you eat a "fish supper" ';
-console.log(a);
-const b = 'Let it be some "old style" thing';
-console.log(b);
+let song = "Fight with the youth";
+let fullScore = 10;
+let myScore = 9;
+let output = `I like the song ${song}. I gave it a score of ${
+	(myScore / fullScore) * 100
+}%.`;
+console.log(output);
+output = `I like the song
+I gave it a score 90%.`;
+console.log(output);
+output = " I like the song.\n I gave it a score 90%";
+console.log(output);
