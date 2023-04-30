f6b8f5df2f2db81094da4e5588016f580dd35dba
olepak <oleksandr.lepak@gmail.com>
2023-04-30 15:53:25 +0300

    upd

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-let song = "Fight with the youth";
-let fullScore = 10;
-let myScore = 9;
-let output = `I like the song ${song}. I gave it a score of ${
-	(myScore / fullScore) * 100
-}%.`;
-console.log(output);
-output = `I like the song
-I gave it a score 90%.`;
-console.log(output);
-output = " I like the song.\n I gave it a score 90%";
-console.log(output);
+let text = "I love cats. Cats are very easy to love. Cats are very popular.";
+console.log(text);
+text = text.replaceAll("cats", "dogs");
+text = text.replaceAll("Cats", "Dogs");
+console.log(text);
