b8459a9e950e4017727677bc8c6791f75ccdc4c7
olepak <oleksandr.lepak@gmail.com>
2023-04-30 18:26:11 +0300

    upd

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let text = "I love cats. Cats are very easy to love. Cats are very popular.";
-console.log(text);
-text = text.replaceAll("cats", "dogs");
-text = text.replaceAll("Cats", "Dogs");
-console.log(text);
+let text = "HELLO WORLD";
+let char = text.charAt(0);
+console.log(char);
+let char2 = text.charCodeAt(0);
+console.log(char2);
+let char3 = text[0];
+console.log(char3);
+let text4 = text.split(" ");
+console.log(text4);
