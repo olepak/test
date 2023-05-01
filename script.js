028b1c8c2c87a992fcf382a0317a395b353ab4b8
olepak <oleksandr.lepak@gmail.com>
2023-05-01 07:24:37 +0300

    upd

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let text = "HELLO WORLD";
-let char = text.charAt(0);
-console.log(char);
-let char2 = text.charCodeAt(0);
-console.log(char2);
-let char3 = text[0];
-console.log(char3);
-let text4 = text.split(" ");
-console.log(text4);
+let text = "Hello world, welcome to the universe";
+let result = text.indexOf("world");
+console.log(result);
