bda199fff9482f142017358ca4e7c8e5ccba1883
olepak <oleksandr.lepak@gmail.com>
2023-05-08 14:42:03 +0300

    upd

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-console.log(` 5 > 4
-${5 > 4}`);
-console.log(` apple > pineapple
-${"apple" > "pineapple"}`);
-console.log(`"2" > "12"
-${"2" > "12"}`);
-console.log(`undefined == null
-${undefined == null}`);
-console.log(`undefined === null
-${undefined === null}`);
-console.log(`"01" == 1
-${"01" == 1}`);
-console.log(`"01" === 1
-${"01" === 1}`);
+let time = prompt("what time is now");
+let greetings;
+if (time < 18) {
+	greetings = "Good day";
+} else if (time > 20) {
+	greetings = "Good night!";
+} else {
+	greetings = "Good evening";
+}
+console.log(greetings);
