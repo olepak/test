804efb9a3dd51617a01ea208d3810200c77eda56
olepak <oleksandr.lepak@gmail.com>
2023-05-08 14:07:07 +0300

    upd

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let a = 0;
-console.log(Boolean(a));
-let b = "0";
-console.log(Boolean(b));
-alert(a == b);
+console.log(` 5 > 4
+${5 > 4}`);
+console.log(` apple > pineapple
+${"apple" > "pineapple"}`);
+console.log(`"2" > "12"
+${"2" > "12"}`);
+console.log(`undefined == null
+${undefined == null}`);
+console.log(`undefined === null
+${undefined === null}`);
+console.log(`"01" == 1
+${"01" == 1}`);
+console.log(`"01" === 1
+${"01" === 1}`);
