42c8aeed5271fa960b50106ac1005b173f8c6523
olepak <oleksandr.lepak@gmail.com>
2023-05-12 17:05:28 +0300

    upd

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-let login = prompt("Enter login", "");
-// check login
-if (login === "Admin") {
-	// enter password
-	let pass = prompt("Enter password", "");
-	// check password
-	if (pass === "TheMaster") {
-		alert("Welcome!");
-	}
-	// show "Canceled" on pressing ESC or empty input
-	else if (pass === null || pass === "") {
-		alert("Canceled");
-	}
-	// show "Canceled" on another string input
-	else {
-		alert("Wrong password");
-	}
-}
-// show "Canceled" on pressing ESC or empty input
-else if (login === null || login === "") {
-	alert("Canceled");
-}
-// show "I don't know you" on another string input
-else {
-	alert("I don't know you");
+let cheese = prompt("What kind of cheese do you want");
+if (cheese) {
+	console.log("Yay! Cheese available for making cheese on toast.");
+} else {
+	console.log("No cheese for you today");
 }
