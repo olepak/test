9a4a697195564e41d851ae4ec5d59e82ab575dc6
olepak <oleksandr.lepak@gmail.com>
2023-05-16 10:19:02 +0300

    upd

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-let age = prompt("What is your age?");
 let message =
-	age < 3
-		? "Hi, baby!"
-		: age < 18
+	login == "Employee"
 		? "Hello"
-		: age < 100
+		: login == "Director"
 		? "Greetings"
-		: "What an unusual age!";
-alert(message);
-console.log(age);
-console.log(typeof age);
-console.log(Boolean(age));
-console.log(Boolean(message));
-console.log(false < 3);
+		: login == ""
+		? "No login"
+		: "";
