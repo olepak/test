3ec5123a22b11e463f83e9bf336323458ac9510d
olepak <oleksandr.lepak@gmail.com>
2023-05-17 07:34:52 +0300

    upd

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let message =
-	login == "Employee"
-		? "Hello"
-		: login == "Director"
-		? "Greetings"
-		: login == ""
-		? "No login"
-		: "";
-console.log("some text");
+let a = prompt("Enter value");
+// a = +a;
+switch (a) {
+	case 4:
+		alert("Exactly");
+		break;
+	case 3:
+	case 5:
+		alert("Wrong!");
+		alert("Why dont't you take a math class?");
+		break;
+	default:
+		alert("Your result is strange. Really");
+}
+console.log(a);
+
+console.log(typeof a);
