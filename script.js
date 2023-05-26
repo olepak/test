3de85dd061f4857e99a03ee1b8e3c28d83a36b74
olepak <oleksandr.lepak@gmail.com>
2023-05-26 15:55:35 +0300

    upd

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let x = 0;
-for (let i = 0; i < 10; i++) {
-	x += i;
-	console.log(x);
+const myArray = [1, 2, 3];
+const doubledArray = myArray.map((item) => item * 2);
+console.log("it was arrow function", doubledArray);
+// My favorite animal function
+
+function myFavoriteAnimal(animal) {
+	return animal + " " + "is my favorite animal";
 }
+alert(myFavoriteAnimal(prompt("What is your favorite animal?")));
