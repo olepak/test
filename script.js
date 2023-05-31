cac2e25b93382508d5684c4679e7ab85d8d59b8d
olepak <oleksandr.lepak@gmail.com>
2023-05-31 07:29:39 +0300

    upd

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-alert(myFunction(prompt("What is your favorite animal?")));
-function myFunction(animal) {
-	return animal + "is my favorite animal";
+function doNothing() {
+	return;
 }
-// function expression
-alert(
-	(function (animal) {
-		return animal + " " + "this is how function expression works";
-	})(prompt("what is your func expression?"))
-);
-// arrow function
-alert(
-	((animal) => animal + " " + "is arrow function")(
-		prompt("What is your favorite arrow")
-	)
-);
+console.log(doNothing() === undefined);
