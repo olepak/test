8142570c2a6aa1d97cf9d039d0011bc6353a5fa7
olepak <oleksandr.lepak@gmail.com>
2023-06-05 19:23:44 +0300

    upd

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function doNothing() {
-	return;
+/* task 1
+function add7(a) {
+	return +a + 7;
 }
-console.log(doNothing() === undefined);
+console.log(add7(prompt("Enter your number")));
+*/
+/*
+function multiply(c, d) {
+	return c * d;
+}
+console.log(
+	multiply(prompt("Enter the first argument"), prompt("Enter second argument"))
+);
+*/
+
+/* task 3
+function capitalize(text, callback) {
+	const firstCharacter = text.charAt(0).toUpperCase();
+	const allLowered = text.slice(1).toLowerCase();
+	let transformed = firstCharacter + allLowered;
+	callback(transformed);
+}
+function display(result) {
+	console.log("Here is transformed text:", result);
+}
+capitalize(prompt("Enter your text"), display);
+*/
+//task 4
+function lastletter(text) {
+	let i = text.charAt(text.length - 1);
+	return i;
+}
+console.log(lastletter(prompt("Enter string")));
