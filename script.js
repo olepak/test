b0ff8de1d33cb90f6d09063bd320c93a2161f41c
oleksandr <oleksandr@Ubuntu.myguest.virtualbox.org>
2023-06-14 17:03:10 +0300

    upd

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ for (let i = 1; i <= answer; i++) {
 	}
 	if (i % 3 === 0) {
 		console.log("Fizz");
-	if (i % 5 === 0) {
-		console.log("Fizz");
-	} else{ console.log(i);}
+		if (i % 5 === 0) {
+			console.log("Fizz");
+		} else {
+			console.log(i);
+		}
+	}
 }
