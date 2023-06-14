3952bb7f33f4c9ec57156eb19e080d0ce109b09e
oleksandr <oleksandr@Ubuntu.myguest.virtualbox.org>
2023-06-14 16:53:52 +0300

    upd

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-/* task 1
-function add7(a) {
-	return +a + 7;
-}
-console.log(add7(prompt("Enter your number")));
-*/
-/*
-function multiply(c, d) {
-	return c * d;
-}
-console.log(
-	multiply(prompt("Enter the first argument"), prompt("Enter second argument"))
+let answer = parseInt(
+	prompt("Please enter the number you would like to FizzBuzz up to:")
 );
-*/
-
-/* task 3
-function capitalize(text, callback) {
-	const firstCharacter = text.charAt(0).toUpperCase();
-	const allLowered = text.slice(1).toLowerCase();
-	let transformed = firstCharacter + allLowered;
-	callback(transformed);
+for (let i = 1; i <= answer; i++) {
+	if (i % 5 === 0 && i % 3 === 0) {
+		console.log("FizzBuzz");
+	}
+	if (i % 3 === 0) {
+		console.log("Fizz");
+	if (i % 5 === 0) {
+		console.log("Fizz");
+	} else{ console.log(i);}
 }
-function display(result) {
-	console.log("Here is transformed text:", result);
-}
-capitalize(prompt("Enter your text"), display);
-*/
-//task 4
-function lastletter(text) {
-	let i = text.charAt(text.length - 1);
-	return i;
-}
-console.log(lastletter(prompt("Enter string")));
