ead0196692f70acc3215aae841643262ae2801c0
olepak <oleksandr.lepak@gmail.com>
2023-06-17 13:58:02 +0300

    upd

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-let answer = parseInt(
-	prompt("Please enter the number you would like to FizzBuzz up to:")
-);
-for (let i = 1; i <= answer; i++) {
-	if (i % 5 === 0 && i % 3 === 0) {
-		console.log("FizzBuzz");
-	}
-	if (i % 3 === 0) {
-		console.log("Fizz");
-		if (i % 5 === 0) {
-			console.log("Fizz");
-		} else {
-			console.log(i);
-		}
-	}
+function sumTwoNumbers(a, b, positive, negative) {
+	if (a && b) {
+		a = +a;
+		b = +b;
+		let sum = a + b;
+		positive(sum);
+	} else negative();
 }
+function checkIfNumber(Number) {
+	if (!isNaN(Number)) {
+		return Number;
+	} else return false;
+}
+function showSum(result) {
+	alert("Here is the result: " + result);
+}
+function showNotaNumber() {
+	alert("You are fucking idiot");
+}
+sumTwoNumbers(
+	checkIfNumber(prompt("Enter first nunber")),
+	checkIfNumber(prompt("Enter second number")),
+	showSum,
+	showNotaNumber
+);
