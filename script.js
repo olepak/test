3cb86fcc24e5b021c415a34f7883fdf5d57a4574
olepak <oleksandr.lepak@gmail.com>
2023-08-11 11:40:54 +0300

    upd

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const fruits = [
-	"Banana",
-	"Orange",
-	"Apple",
-	"Mango",
-	"Peach",
-	"Melon",
-	"Watermelon",
-];
-let fLen = fruits.length;
-console.log(fLen);
-for (let i = 0; i < fLen; i++) {
-	console.log(fruits[i]);
-}
+const points = [40, 100, 1, 5, 25, 10];
+console.log(points);
+
+const points2 = new Array(40, 100, 1, 5, 25, 10);
+console.log(points2);
