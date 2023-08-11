377731cf45afaf4d49317701d036773ddfc52ece
olepak <oleksandr.lepak@gmail.com>
2023-08-11 06:46:53 +0300

    upd

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const someArray = [123, 43434, 44343, 343434, 3434340980948384];
-console.log(someArray.length);
-let number = someArray[3];
-console.log(number);
-number = someArray[0];
-console.log(number);
-number = someArray[someArray.length - 1];
-console.log(number);
+const fruits = [
+	"Banana",
+	"Orange",
+	"Apple",
+	"Mango",
+	"Peach",
+	"Melon",
+	"Watermelon",
+];
+let fLen = fruits.length;
+console.log(fLen);
+for (let i = 0; i < fLen; i++) {
+	console.log(fruits[i]);
+}
