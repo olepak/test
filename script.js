a177dc36c17b2a7f684c9d4eb072b2a24dcdce3f
olepak <oleksandr.lepak@gmail.com>
2023-08-17 17:24:19 +0300

    upd

--- script.js
+++ script.js
@@ -4,31 +4,21 @@ function outputPrimeNumbers() {
 	if (isNaN(n)) {
 		alert("Incorrect input");
 	} else {
-		let accumulated = "";
-		let divisorArray = [];
+		const divArray = [];
+		let primeNumbers = "";
 		for (let i = 2; i <= n; i++) {
 			for (let j = 1; j <= i; j++) {
-				if (i % j == 0) {
-					divisorArray.push(j);
+				let divideResult = i % j;
+				if (divideResult === 0) {
+					divArray.push(j);
 				} else {
 					continue;
 				}
+				primeNumbers = +` ${i}`;
 			}
-			if ((divisorArray.length = 2)) {
-				accumulated += ` ${i}`;
-			}
-
-			console.log(accumulated);
 		}
+		console.log(primeNumbers);
 	}
-	alert("done");
 }
 
 outputPrimeNumbers();
-/*
-n = 1 2 3 4 5 6 7 - given interval
-
-take every number from an interval starting from i=2 and check against all numbers starting from j=1 to itself
-whether or not it divides without reminder
-
-*/
