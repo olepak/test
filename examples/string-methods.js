3167e87e71ca81856cbe8c2530e12de3be3bfa6e
olepak <oleksandr.lepak@gmail.com>
2023-09-12 07:21:30 +0300

    upd

---
+++ examples/string-methods.js
@@ -0,0 +1,14 @@
+// at() method returns an indexed character from a string
+// it returns the same as []
+// string.at(index)
+// index - optional. The index of the character to be returned. Default is 0. -1 returns the last character
+// return value - the character in given position
+function at() {
+	let text = "W3Schools";
+	let char = text.at(0);
+	console.log(char);
+	// returns the same as []
+	let char2 = text[3];
+	console.log(char2);
+}
+at();
