4f5d86036ed877edbefb63fa409dea0bd090af54
olepak <oleksandr.lepak@gmail.com>
2023-09-26 10:03:54 +0300

    upd

--- examples/argument-objects.js
+++ examples/argument-objects.js
@@ -1,1 +1,2 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
 // arguments is an array-like object accessible inside functions that contains the values of the arguments
