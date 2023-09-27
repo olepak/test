cd23324432333a494c466ed3873d3b86a0392856
olepak <oleksandr.lepak@gmail.com>
2023-09-27 08:53:25 +0300

    upd

--- script.js
+++ script.js
@@ -10,4 +10,4 @@ function squareNumbers(numbers) {
 function showResult(result) {
 	console.log(result);
 }
-showSquaredNumbers(squareNumbers([1, 450, 4, 5, 6]), showResult); //?
+showSquaredNumbers(squareNumbers([1, 450, 4, 5, 6]), showResult);
