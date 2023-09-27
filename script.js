48fde846466a8a4fc56ccb0e94ef4c300c2d22e5
olepak <oleksandr.lepak@gmail.com>
2023-09-27 07:15:48 +0300

    upd

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let char = String.fromCharCode(128007);
-console.log(char);
-let char2 = String.fromCodePoint(128077);
-console.log(char2);
-let char3 = String.fromCharCode(79, 107, 32, 55357, 56397);
-console.log(char3);
-let char4 = String.fromCodePoint(79, 107, 32, 128077);
-console.log(char4);
+function showSquaredNumbers(square, output) {
+	output(square);
+}
+
+function squareNumbers(numbers) {
+	return numbers.reduce(function (sum, n) {
+		return sum + n * n;
+	}, 0);
+}
+function showResult(result) {
+	console.log(result);
+}
+showSquaredNumbers(squareNumbers([1, 450, 4, 5, 6]), showResult); //?
