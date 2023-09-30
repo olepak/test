97c48d16ee2a7647d4d78b4e8b220c7b746dd2e7
olepak <oleksandr.lepak@gmail.com>
2023-09-30 08:55:11 +0300

    upd

--- examples/rest-parameters.js
+++ examples/rest-parameters.js
@@ -1,3 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
 // the rest parameter syntax allows a function to accept an indefinite  number of an arguments as an array, providing a way to represent variadic functions in javascript
 function sum(...theArgs) {
 	let total = 0;
@@ -11,3 +12,12 @@ console.log(sum(1, 2, 3, 4));
 
 // syntax:
 function f(a, b, ...theArgs) {}
+
+// DESCRIPTION
+// a function definition's last parameter can be prefixed with "..." which will cause all remaining (user supplied) parameters to be placed within an Array object
+function myFun(a, b, ...manyMoreArgs) {
+	console.log("a", a);
+	console.log("b", b);
+	console.log("manyMoreArgs", manyMoreArgs);
+}
+myFun("one", "two", "three", "four", "five", "six");
