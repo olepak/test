34e75c80302bc1e4c230bdcb2c8bda85e43a750b
olepak <oleksandr.lepak@gmail.com>
2023-10-05 15:59:52 +0300

    upd

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function getCount(str) {
-	let result = str.match(/[aeiou]/gi) || [];
-	console.log(result.length);
+function findNeedle(haystack) {
+	return "found the needle at position " + haystack.indexOf("needle");
 }
-getCount("my pyx");
+console.log(
+	findNeedle([
+		"283497238987234",
+		"a dog",
+		"a cat",
+		"some random junk",
+		"a piece of hay",
+		"needle",
+		"something somebody lost a while ago",
+	])
+);
+console.log(
+	findNeedle(["3", "123124234", undefined, "world", "hay", 2, "3", true, false])
+);
