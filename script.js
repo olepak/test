c592732f958ed41f32cd515681931f45f3eff8bc
olepak <oleksandr.lepak@gmail.com>
2023-10-08 18:26:50 +0300

    start DOM

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-function findNeedle(haystack) {
-	return "found the needle at position " + haystack.indexOf("needle");
-}
-console.log(
-	findNeedle([
-		"283497238987234",
-		"a dog",
-		"a cat",
-		"some random junk",
-		"a piece of hay",
-		"needle",
-		"something somebody lost a while ago",
-	])
-);
-console.log(
-	findNeedle(["3", "123124234", undefined, "world", "hay", 2, "3", true, false])
-);
+const div = document.createElement("div");
+document.body.appendChild(div);
+div.style.color = "blue";
+div.setAttribute("style", "color: blue; background: white;");
+div.textContent = "Hello world!";
