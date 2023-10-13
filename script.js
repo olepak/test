4092824de8be7a0f5a5c584f314dd0b7a0da845c
olepak <oleksandr.lepak@gmail.com>
2023-10-13 16:24:03 +0300

    upd

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const div = document.createElement("div");
-document.body.appendChild(div);
-div.style.color = "blue";
-div.setAttribute("style", "color: blue; background: white;");
-div.textContent = "Hello world!";
+function countPositivesSumNegatives(input) {
+	const positive = input
+		.filter((value) => value > 0)
+		.reduce((sum, value) => sum + value);
+	const negative = input
+		.filter((value) => value < 0)
+		.reduce((sum, value) => sum + value);
+	const arr = [];
+	arr[0] = positive;
+	arr[1] = negative;
+	return arr;
+}
+console.log(
+	countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13])
+);
+
+function moreConciseCount(input) {
+	const arr = input.reduce(
+		(acc, value) => {
+			if (value > 0) {
+				acc[0] += value;
+			} else {
+				acc[1] += value;
+			}
+			return acc;
+		},
+		[0, 0]
+	);
+	return arr;
+}
+
+console.log(moreConciseCount([1, 2, 3, 4, 5, 6, 7, 8, 10, -11, -12, -13, -14]));
