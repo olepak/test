11c81f3ff5c05f597a81994774a0fd2b8724f37f
olepak <oleksandr.lepak@gmail.com>
2023-10-13 17:04:16 +0300

    upd

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-function countPositivesSumNegatives(input) {
-	const positive = input
-		.filter((value) => value > 0)
-		.reduce((sum, value) => sum + value);
-	const negative = input
-		.filter((value) => value < 0)
-		.reduce((sum, value) => sum + value);
-	const arr = [];
-	arr[0] = positive;
-	arr[1] = negative;
-	return arr;
-}
-console.log(
-	countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13])
-);
-
 function moreConciseCount(input) {
-	const arr = input.reduce(
+	if (input === null || input.length === 0) {
+		return [];
+	}
+	return input.reduce(
 		(acc, value) => {
 			if (value > 0) {
-				acc[0] += value;
+				acc[0] += 1;
 			} else {
 				acc[1] += value;
 			}
@@ -26,7 +13,7 @@ function moreConciseCount(input) {
 		},
 		[0, 0]
 	);
-	return arr;
 }
 
 console.log(moreConciseCount([1, 2, 3, 4, 5, 6, 7, 8, 10, -11, -12, -13, -14]));
+console.log(moreConciseCount([]));
