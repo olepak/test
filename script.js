db1b99159869719a9adead0f5934df47a94236dc
olepak <oleksandr.lepak@gmail.com>
2024-01-12 09:49:19 +0200

    upd

---
+++ script.js
@@ -0,0 +1,4 @@
+const parentElement = document.createElement("div");
+document.body.appendChild(parentElement);
+const nestedDiv = document.createElement("div");
+parentElement.appendChild(nestedDiv);
